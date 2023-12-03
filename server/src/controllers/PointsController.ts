854bc4cbaa141e12a4787aa8041e5f1717ee1771
Rubendvb <rubenvasquez556@gmail.com>
2023-12-02 23:28:57 -0300

    add image fake

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -50,7 +50,8 @@ class PointsController {
     const trx = await knex.transaction()
 
     const point = {
-      image: 'image-fake',
+      image:
+        'https://images.unsplash.com/photo-1574202611423-a028032f0a13?q=60&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       name,
       email,
       whatsapp,
