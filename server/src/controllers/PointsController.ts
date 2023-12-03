73b2ddf308ee7a67c4c99dec9422dccd9510d5a4
Rubendvb <rubenvasquez556@gmail.com>
2023-12-02 22:57:02 -0300

    specific item route created

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,24 @@ import knex from '../database/connection'
 import { Request, Response } from 'express'
 
 class PointsController {
+  async index(req: Request, res: Response) {
+    const { city, uf, items } = req.query
+
+    const parsedItems = String(items)
+      .split(',')
+      .map((item) => Number(item.trim()))
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*')
+
+    res.json(points)
+  }
+
   async show(req: Request, res: Response) {
     const { id } = req.params
 
@@ -54,6 +72,8 @@ class PointsController {
     })
 
     await trx('point_items').insert(pointItems)
+
+    await trx.commit()
 
     return res.json({
       id: point_id,
